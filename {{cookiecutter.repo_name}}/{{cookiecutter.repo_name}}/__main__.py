5fa12bc2e8942c4c0f833aabcb4f7031803f7efc
Philipp Gayret <philipp@hileco.com>
2023-07-04 00:15:34 +0200

    Add main.main

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__main__.py
@@ -1,6 +1,10 @@
 import fire
 from {{cookiecutter.repo_name}}.main import CLI
-import sys
+
+
+def main():
+    fire.Fire(component=CLI, name="{{cookiecutter.repo_name}}")
+
 
 if __name__ == "__main__":
-    fire.Fire(component=CLI, name="{{cookiecutter.repo_name}}")
+    main()
